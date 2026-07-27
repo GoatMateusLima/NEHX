3ff85ec4d25d269267d66d6b217abc303ab14a79
Mateus lima Bispo <mateuslimabispo15@gmail.com>
2026-07-26 21:33:22 -0300

    cpnseguiiii manipular o DOMMMMMMMMMMMMMMMMMMMM

--- att_Tema2_ManipulacaoDOM/script.js
+++ att_Tema2_ManipulacaoDOM/script.js
@@ -1,5 +1,4 @@
 
-
 const a = document.querySelector('#a')
 a.textContent = 'h1'
 
@@ -25,7 +24,7 @@ btn.addEventListener('click', () => {
 const users = []
 const cadastro = document.getElementById('fmCadastro')
 
-cadastro.addEventListener('submit', (event)=> {
+cadastro.addEventListener('submit', (event) => {
     event.preventDefault()
 
     const dados = new FormData(cadastro)
@@ -34,7 +33,7 @@ cadastro.addEventListener('submit', (event)=> {
     let email = dados.get('email')
     let numero = dados.get('numero')
     let senha = dados.get('senha')
-    users.push({name,idade,email,numero,senha})
+    users.push({ name, idade, email, numero, senha })
     console.log(users)
     renderList()
 })
@@ -43,14 +42,51 @@ cadastro.addEventListener('submit', (event)=> {
 const list = document.getElementById('list')
 function renderList() {
     list.innerHTML = ""
-    let element = document.createElement('ul')
-    users.forEach((a,i)=>{
-        element.innerHTML = `
-        <li>${a.name}<button type="click" onclick=${removeList(i)}>X</button></li>`;
+    users.forEach((a, i) => {
+        //const i = createElement('li')
+        //i.textContent(a.name)
+        //list.appendChild(i)
+        //element.innerHTML = `
+        //<li>${a.name}<btnRemover type="click" onclick="removeList()">X</btnRemover></li>`;
+        let element = document.createElement('li')
+        element.textContent = a.name
+
+
+
+
+        let btnEditar = document.createElement('button') 
+        btnEditar.textContent = "E"
+        btnEditar.addEventListener('click', (event) => {
+            event.preventDefault()
+
+            const dados = new FormData(cadastro)
+            let name = dados.get('name')
+            let idade = dados.get('idade')
+            let email = dados.get('email')
+            let numero = dados.get('numero')
+            let senha = dados.get('senha')
+
+            name == '' ? name = a.name : name = name
+            idade == '' ? idade = a.idade : idade = idade
+            email == '' ? email = a.email : email = email
+            numero == '' ? numero = a.numero : numero = numero
+            senha == '' ? senha = a.senha : senha = senha
+
+            users.splice(i, 1, { name, idade, email, numero, senha })
+            renderList()
+        })
+
+
+        let btnRemover = document.createElement('button') // cria  um botao no documento
+        btnRemover.textContent = 'X' //define o texto do meu botao
+        btnRemover.addEventListener('click', (event) => {
+            event.preventDefault() // previne o comportamento padrão do botão
+            users.splice(i, 1)
+            renderList()  //renderiza a lista novamente após remover o usuário
+        })
+        element.appendChild(btnEditar)
+        element.appendChild(btnRemover)
         list.appendChild(element)
     })
-    
-}
-function removeList(indice){
-    users.splice(indice, 1)
+
 }
